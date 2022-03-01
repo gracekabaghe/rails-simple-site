ff0fecbe82eb1f4af69d14bb4fb6059fa7a20796
gracekabaghe <gracema2002@gmail.com>
2022-03-01 20:21:38 +0200

    Fix rubocop

--- db/migrate/20220224102537_create_comments.rb
+++ db/migrate/20220224102537_create_comments.rb
@@ -1,11 +1,11 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
-    unless table_exists? :comments
-     create_table :comments do |t|
-       t.text :text
-       t.belongs_to :post, index: true
-       t.timestamps
-     end
-   end
- end
+    return if table_exists? :comments
+
+    create_table :comments do |t|
+      t.text :text
+      t.belongs_to :post, index: true
+      t.timestamps
+    end
+  end
 end
--- db/migrate/20220224102820_create_likes.rb
+++ db/migrate/20220224102820_create_likes.rb
@@ -1,7 +1,5 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
-    unless table_exists? :likes
-     create_table :likes, &:timestamps
-   end
- end
+    create_table :likes, &:timestamps unless table_exists? :likes
+  end
 end
