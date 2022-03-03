50c41eeea3d86e0d211a4f50f28a07d37790f04a
gracekabaghe <gracema2002@gmail.com>
2022-03-03 18:03:13 +0200

    Add name and post counter validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  validates :name, presence: true
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
+
   def self.recent_post(id)
     Post.where(user_id: id).limit(3)
   end
