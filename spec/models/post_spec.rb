53fcc3440c913b0ca0fbf128bf65c3954cdedf00
gracekabaghe <gracema2002@gmail.com>
2022-03-03 18:31:40 +0200

    Add spec for models

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+  subject { Post.create(author: first_user, title: 'Hello', text: 'This is my first post') }
+
+  before { subject.save }
+
+  it 'return the recent comments for a post' do
+    expect(Post.recent_comments(subject.id).count).to eql 0
+  end
+
+  it 'return all the comments for a post' do
+    expect(Post.all_comments(subject.id).count).to eql 0
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
+
+  before { subject.save }
+
+  it 'return the three latest posts for a user' do
+    expect(User.recent_post(subject.id).count).to eql 0
+  end
+
+  it 'return all the posts for a user' do
+    expect(User.all_post(subject.id).count).to eql 0
+  end
 end
