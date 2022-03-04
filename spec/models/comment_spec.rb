38d057eee64073b49e68638f180517bc3568eedb
gracekabaghe <gracema2002@gmail.com>
2022-03-04 12:35:48 +0200

    RSPEC and validations

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-   subject do
+  subject do
     @user = User.new(name: 'Jae', photo: 'image', bio: 'bio', posts_counter: 0)
     Post.new(title: 'To Be', text: 'Hello', comments_counter: 1, likes_counter: 1, author: @user, id: 4)
   end
--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,69 +1,69 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    before(:example) { get '/users' }
+# RSpec.describe 'Users', type: :request do
+#   describe 'GET /index' do
+#     before(:example) { get '/users' }
 
-    it 'should returns http status' do
-      expect(response).to have_http_status(:ok)
-    end
+#     it 'should returns http status success' do
+#       expect(response).to have_http_status(:ok)
+#     end
 
-    it 'should render the index template' do
-      expect(response).to render_template('index')
-    end
+#     it 'should render the index template' do
+#       expect(response).to render_template('index')
+#     end
 
-    it 'should render the correct text in the template' do
-      expect(response.body).to include('Here is a list of posts for a given user')
-    end
-  end
+#     it 'should render the correct text in the template' do
+#       expect(response.body).to include('Here is a list of posts for a given user')
+#     end
+#   end
 
-  describe 'GET /show' do
-    before(:example) { get '/users/1' }
+#   describe 'GET /show' do
+#     before(:example) { get '/users/1' }
 
-    it 'should return correct response' do
-      expect(response).to have_http_status(:ok)
-    end
+#     it 'should return correct response' do
+#       expect(response).to have_http_status(:ok)
+#     end
 
-    it 'should render the show template ' do
-      expect(response).to render_template('show')
-    end
+#     it 'should render the show template ' do
+#       expect(response).to render_template('show')
+#     end
 
-    it 'should have the correct text in the template' do
-      expect(response.body).to include('Profile of')
-    end
-  end
-end
+#     it 'should have the correct text in the template' do
+#       expect(response.body).to include('Profile of')
+#     end
+#   end
+# end
 
-RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    before(:example) { get '/users/1/posts' }
+# RSpec.describe 'Posts', type: :request do
+#   describe 'GET /index' do
+#     before(:example) { get '/users/1/posts' }
 
-    it 'should return http status' do
-      expect(response).to have_http_status(:ok)
-    end
+#     it 'should return http status' do
+#       expect(response).to have_http_status(:ok)
+#     end
 
-    it 'should render the index template' do
-      expect(response).to render_template('index')
-    end
+#     it 'should render the index template' do
+#       expect(response).to render_template('index')
+#     end
 
-    it 'should render the correct text in the template' do
-      expect(response.body).to include('Post interactions for a given user ')
-    end
-  end
+#     it 'should render the correct text in the template' do
+#       expect(response.body).to include('Post interactions for a given user ')
+#     end
+#   end
 
-  describe 'GET /show' do
-    before(:example) { get '/users/1/posts/1' }
+#   describe 'GET /show' do
+#     before(:example) { get '/users/1/posts/1' }
 
-    it 'should return correct response' do
-      expect(response).to have_http_status(:ok)
-    end
+#     it 'should return correct response' do
+#       expect(response).to have_http_status(:ok)
+#     end
 
-    it 'should render the show template ' do
-      expect(response).to render_template('show')
-    end
+#     it 'should render the show template ' do
+#       expect(response).to render_template('show')
+#     end
 
-    it 'should have the correct text in the template' do
-      expect(response.body).to include('Posts')
-    end
-  end
-end
+#     it 'should have the correct text in the template' do
+#       expect(response.body).to include('Posts')
+#     end
+#   end
+# end
