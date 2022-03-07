5ae01ed1625095ef742625998c21a99835358e2a
gracekabaghe <gracema2002@gmail.com>
2022-03-07 18:13:49 +0200

    Installed devise gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :posts
   has_many :comments
   has_many :likes
 
   validates :name, presence: true
-  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
+  # validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
 
   def self.recent_post(id)
     Post.where(user_id: id).limit(3)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create] do
       resources :comments, only: %i[create new]
@@ -8,5 +9,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'users#index'
+  root to: 'users#index'
 end
