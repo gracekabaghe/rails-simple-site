5fcda90c105dd54aafd742849c834f71cd25ee64
gracekabaghe <gracema2002@gmail.com>
2022-03-08 10:51:28 +0200

    Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do 
+    get '/users/sign_out' => 'devise/sessions#destroy' 
+  end
+  
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create] do
       resources :comments, only: %i[create new]
