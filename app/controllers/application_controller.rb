bb358b35ab43ffea621b2cf7a629a00cc48c67aa
luftedar <ugurorcun91@gmail.com>
2022-03-11 18:41:28 +0300

    Fix Bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   before_action :authenticate_with_token
   before_action :authenticate_user!
-  protect_from_forgery with: :exception
   before_action :update_allowed_parameters, if: :devise_controller?
 
   protected
